c1844847129cf055ab7f99a21e0aa57058538e8f
Sungwoo Kim <iam@sung-woo.kim>
2024-08-06 17:47:50 -0400

    add ndss crawler

---
+++ data_crawler/crawler.js
@@ -0,0 +1,82 @@
+const resourse = require('./resources');
+const fs = require('fs/promises')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const pick = (n = 0, arr = []) => arr.filter((_, i) => i < n)
+
+const defaultLoc = {
+  year: 0,
+  venue: "",
+  url: "",
+  unitQs: "",
+  titleQs: "",
+  absQs: [""],
+}
+// Data Paper = Paper title abstract
+// crawl :: Locator -> ([Paper] -> [error])
+const crawl = async (loc = defaultLoc) => {
+  const res = await fetch(loc.url)
+  if (!res.ok) { return [[], [new Error(`${res.statusText} ${loc.url}`)]]}
+  const html = await res.text()
+  const document = (new JSDOM(html)).window.document
+  const units = document.querySelectorAll(loc.unitQs)
+
+  let papers = []
+  let errors = []
+  for (let unit of units) {
+    const title = unit.querySelector(loc.titleQs)?.textContent?.trim()
+    if (!title) { 
+      errors = [...errors, new Error(`cannot find title for ${loc.url} ${loc.titleQs}`)]
+      continue
+    }
+    const abstract = loc.absQs.reduce((acc, cur) =>
+      acc === null ?
+      unit.querySelector(cur)?.textContent?.trim() :
+      acc
+    , null)
+    if (!abstract) {
+      errors = [...errors, new Error(`cannot find abstrct for ${loc.url} ${title} ${loc.absQs}`)]
+    }
+    papers = [
+      ...papers,
+    {
+      title,
+      abstract: abstract || ""
+    }]
+  }
+  return [papers, errors]
+};
+
+// crawl :: year -> venue -> title -> abstract -> error
+const save = async (year = 0, venue = "", title = "", abstract = "") => {
+  // const fs = {
+  //   mkdir: console.log,
+  //   writeFile: console.log,
+  // }
+  const newTitle = title.replaceAll("/", " ")
+  let err = await fs.mkdir(`data/${year}/${venue}`, { recursive: true })
+  if (err !== undefined) { return err }
+  err = await fs.writeFile(`data/${year}/${venue}/${newTitle}`, abstract)
+  if (err !== undefined) { return err }
+  
+  return null
+}
+
+(async () => {
+resourse.map(async (r) => {
+  const [papers, errs] = await crawl(r)
+  // 
+  if (0 < errs.length) {
+    console.log(`${errs.length} errors in crawling ${r.url}`)
+    console.log("show some: ")
+    pick(3, errs).map(e => console.log(e))
+  }
+  papers.map(async (p) => {
+    const err = await save(r.year, r.venue, p.title, p.abstract)
+    if (err) {
+      console.error("failed to save ", err)
+    }
+  })
+})
+})();
---
+++ data_crawler/resources.js
@@ -0,0 +1,52 @@
+const seq = (start, end, step = 1) => Array.from({ length: end - start }, (_, i) => start + i * step)
+const notN = (n, i) => n !== i
+const not2020 = i => notN(2020, i)
+
+// Data Locator = Locator Int   String String String  String   [String]
+//                        ^year ^url   ^venue ^unitQS ^titleQS ^absQS and (Spares)
+const ndss = [
+  {
+    year: 2018,
+    venue: "ndss",
+    url: `https://www.ndss-symposium.org/ndss-program-2018-program/`,
+    unitQs: "div.row > div.col-lg-8",
+    titleQs: "p > b",
+    absQs: ["div.modal-body"],
+  },
+  {
+    year: 2019,
+    venue: "ndss",
+    url: `https://www.ndss-symposium.org/ndss-program/ndss-symposium-2019-program/`,
+    unitQs: "ul.list-group-session > li",
+    titleQs: "div.row > div.col-10 > div > strong",
+    absQs: ["div.row > div.col-12"],
+  },
+  {
+    year: 2020,
+    venue: "ndss",
+    url: `https://www.ndss-symposium.org/ndss-program/2020-program/`,
+    unitQs: "ul.list-group-session > li",
+    titleQs: "div.row > div.col-10 > div > strong",
+    absQs: ["div.row > div.col-12"],
+  },
+  ...seq(2021, 2023).map(y => ({
+  year: y,
+  venue: "ndss",
+  url: `https://www.ndss-symposium.org/ndss-program/ndss-${y}/`,
+  unitQs: "ul.list-group-session > li",
+  titleQs: "div.row > div.col-10 > div > strong",
+  absQs: ["div.row > div.col-12"],
+  })),
+  {
+    year: 2023,
+    venue: "ndss",
+    url: `https://www.ndss-symposium.org/ndss-program/symposium-2023/`,
+    unitQs: "ul.list-group-session > li",
+    titleQs: "div.row > div.col-10 > div > strong",
+    absQs: ["div.row > div.col-12"],
+  }
+]
+
+module.exports = [
+  ...ndss
+]
