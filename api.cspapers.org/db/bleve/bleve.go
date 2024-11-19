swkim101/cspapers.org

commit fa08ba4fde0d1193d5f20d0b2cd20338b347a3e6

api: support MUST options
We support must options, embraced by doulbe quotes.
The results must contain "this".

Related to #4

api.cspapers.org/db/bleve/bleve.go
@@ -148,8 +148,18 @@ func search(req *types.SearchRequest) *types.SearchResponse {
 	}
 
 	keyword := bleve.NewDisjunctionQuery(keywordQuery...)
+	cquery := bleve.NewConjunctionQuery(year, venue, keyword)
 
-	query := bleve.NewConjunctionQuery(year, venue, keyword)
+	query := bleve.NewBooleanQuery()
+	query.AddMust(cquery)
+
+	if len(req.Must) != 0 {
+		for _, word := range req.Must {
+			log.Debugf("must: ", word)
+			q := bleve.NewMatchQuery(word)
+			query.AddMust(q)
+		}
+	}
 
 	log.Debugf("%v", req.Query)
 	search := bleve.NewSearchRequestOptions(query, 20, int(req.Skip), false)

api.cspapers.org/server/server.go
@@ -98,6 +98,7 @@ func unmarshal(query url.Values) (*types.SearchRequest, error) {
 	}
 	return &types.SearchRequest{
 		Query:     query.Get("query"),
+		Must:      strings.Split(query.Get("must"), ","),
 		OrderBy:   query.Get("orderBy"),
 		Ascending: query.Get("ascending") == "true",
 		Venue:     venue,

api.cspapers.org/types/types.go
@@ -42,6 +42,7 @@ type SearchRequest struct {
 	YearFrom  uint32   `json:"yearFrom"`
 	YearTo    uint32   `json:"yearTo"`
 	Skip      uint32   `json:"skip"`
+	Must      []string `json:"must"`
 }
 
 func (p *Paper) ToIndex() string {

src/App.js
@@ -4,8 +4,8 @@ import { conferences } from './conferences';
 import { useEffect, useState } from 'react';
 import Paper from './components/Paper';
 import ConferenceTree from './components/ConferenceTree';
-import { seq } from './functional';
-import { ASCE, CURRENT_YEAR, DATE, MIN_YEAR, SCORE, LAST_UPDATE } from './const';
+import { seq, doubleQuotes } from './functional';
+import { ASCE, CURRENT_YEAR, DATE, MIN_YEAR, SCORE, LAST_UPDATE, CITATION } from './const';
 
 function App() {
   /**
@@ -39,6 +39,7 @@ function App() {
   }
 
   const search = async () => {
+    const must = doubleQuotes(query)
     const req = {
       query,
       yearFrom,
@@ -47,6 +48,7 @@ function App() {
       orderBy,
       ascending,
       skip,
+      must,
     }
     setWaiting(true)
     const [res, err] = await api.search(req)

src/api.js
@@ -9,6 +9,7 @@ import { ABSTRACT_URL, API_HOST } from "./const";
  *   yearFrom:  number,
  *   yearTo:    number,
  *   skip:      number,
+ *   must: string[]
  * }} SearchRequest
  * 
  * @typedef {{

src/functional.js
@@ -27,6 +27,9 @@ export const buildTree = (edges, root) => {
   return t
 }
 
+export const doubleQuotes = (str = "") =>
+  str.split('"').slice(0, -1).filter((_, idx) => idx % 2 === 1)
+
 const buildTree1Depth = (edges = [], parent = "") => ({
   name: parent,
   children: edges.reduce((acc, cur) => fst(cur) === parent ? [...acc, { name: snd(cur) }] : acc, [])

commit 0ada4d8c573e23285abdd2f046ec8f81a036c22c

Fix no query result
fixes: fa08ba4fde0d1193d5f

api.cspapers.org/db/bleve/bleve.go
@@ -148,16 +148,13 @@ func search(req *types.SearchRequest) *types.SearchResponse {
 	}
 
 	keyword := bleve.NewDisjunctionQuery(keywordQuery...)
-	cquery := bleve.NewConjunctionQuery(year, venue, keyword)
-
-	query := bleve.NewBooleanQuery()
-	query.AddMust(cquery)
+	query := bleve.NewConjunctionQuery(year, venue, keyword)
 
 	if len(req.Must) != 0 {
 		for _, word := range req.Must {
-			log.Debugf("must: ", word)
+			log.Debugf("must: %v", word)
 			q := bleve.NewMatchQuery(word)
-			query.AddMust(q)
+			query = bleve.NewConjunctionQuery(query, q)
 		}
 	}
 

api.cspapers.org/server/server.go
@@ -96,9 +96,15 @@ func unmarshal(query url.Values) (*types.SearchRequest, error) {
 			return nil, fmt.Errorf("invalid venue %v", v)
 		}
 	}
+	mustStr := query.Get("must")
+	must := []string{}
+	if strings.TrimSpace(mustStr) != "" {
+		must = strings.Split(mustStr, ",")
+	}
+
 	return &types.SearchRequest{
 		Query:     query.Get("query"),
-		Must:      strings.Split(query.Get("must"), ","),
+		Must:      must,
 		OrderBy:   query.Get("orderBy"),
 		Ascending: query.Get("ascending") == "true",
 		Venue:     venue,