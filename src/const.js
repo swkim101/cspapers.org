472250eca92974fd18efd7377ad7b23aad814151
Sungwoo Kim <iam@sung-woo.kim>
2025-11-03 12:21:02 +0000

    add papers

--- src/const.js
+++ src/const.js
@@ -7,5 +7,5 @@ export const MIN_YEAR = 1960
 
 export const ABSTRACT_URL = import.meta.env.VITE_ABSTRACT_URL
 export const API_HOST = import.meta.env.VITE_API_HOST
-export const LAST_UPDATE = "last update Oct. 09, 2025"
+export const LAST_UPDATE = "last update Nov. 03, 2025"
 export const CITATION = "@misc{kim2024cspapers, title={cspapers.org}, url={https://cspapers.org/}, author={Kim, Sungwoo}, year={2024}, month=aug, language={en} }"
