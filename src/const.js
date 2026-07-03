7fa2e960fc767b8ebbb9f5c723f111a5aa3626b4
Sungwoo Kim <iam@sung-woo.kim>
2026-07-03 02:30:52 +0000

    update papers

--- src/const.js
+++ src/const.js
@@ -7,4 +7,4 @@ export const MIN_YEAR = 1960
 
 export const ABSTRACT_URL = import.meta.env.VITE_ABSTRACT_URL
 export const API_HOST = import.meta.env.VITE_API_HOST
-export const LAST_UPDATE = "last update May 30, 2026"
+export const LAST_UPDATE = "last update July 02, 2026"
